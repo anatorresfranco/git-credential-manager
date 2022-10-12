8232563668881ed21b18956d1a988a0aa9013de0
anatorresfranco <francotorres021021uau@gmail.com>
2022-10-12 13:23:39 -0400

    Delete src directory

--- src/shared/Atlassian.Bitbucket/IUserInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Atlassian.Bitbucket
-{
-    public interface IUserInfo
-    {
-        string UserName{ get; }
-        bool IsTwoFactorAuthenticationEnabled { get; }
-    }
-}
